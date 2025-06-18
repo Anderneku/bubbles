3efc690082028588866e2e8b5658f0429563526f
SiXFeet <chukunekunwanwene@gmail.com>
2025-06-18 22:51:21 +0100

    column

--- lib/pages/get_started.dart
+++ lib/pages/get_started.dart
@@ -20,106 +20,104 @@ class _GetStartedState extends State<GetStarted> {
             Positioned(
               top: 150,
               left: -250,
-              child: Image.asset('bubbleImages/bubble.png'),
+              child: Image.asset('assets/bubbleImages/bubble.png'),
             ),
             Positioned(
               top: 50,
               left: 50,
-              child: Image.asset('bubbleImages/bubble6.png', scale: 2.5),
+              child: Image.asset('assets/bubbleImages/bubble6.png', scale: 2.5),
             ),
             Positioned(
               top: -150,
               right: -250,
-              child: Image.asset('bubbleImages/bubble.png'),
+              child: Image.asset('assets/bubbleImages/bubble.png'),
             ),
+            Column(),
             // White Circle Icon
             Positioned(
               top: 120,
               right: 0,
               left: 0,
               child: Center(
-                child: Image.asset('bubbleImages/bubbleIcon.png', scale: .9),
+                child: Image.asset(
+                  'assets/bubbleImages/bubbleIcon.png',
+                  scale: .9,
+                ),
               ),
             ),
             Positioned(
               bottom: -200,
               left: -290,
-              child: Image.asset('bubbleImages/bubble.png'),
+              child: Image.asset('assets/bubbleImages/bubble.png'),
             ),
             Positioned(
               top: 300,
               right: 10,
-              child: Image.asset('bubbleImages/bubble4.png', scale: 4),
+              child: Image.asset('assets/bubbleImages/bubble4.png', scale: 4),
             ),
             Positioned(
               top: 250,
               right: 90,
-              child: Image.asset('bubbleImages/bubble5.png', scale: 2),
+              child: Image.asset('assets/bubbleImages/bubble5.png', scale: 2),
             ),
             Positioned(
               bottom: 280,
               left: 200,
-              child: Image.asset('bubbleImages/bubble2.png', scale: 4),
+              child: Image.asset('assets/bubbleImages/bubble2.png', scale: 4),
             ),
             Positioned(
               bottom: 230,
               left: 20,
-              child: Image.asset('bubbleImages/bubble2.png', scale: 6),
+              child: Image.asset('assets/bubbleImages/bubble2.png', scale: 6),
             ),
             Positioned(
               bottom: 140,
               left: 50,
-              child: Image.asset('bubbleImages/bubble3.png', scale: 2),
+              child: Image.asset('assets/bubbleImages/bubble3.png', scale: 2),
             ),
             Positioned(
               bottom: -20,
               right: -250,
-              child: Image.asset('bubbleImages/bubble.png'),
+              child: Image.asset('assets/bubbleImages/bubble.png'),
             ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: -50,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'Welcome to ',
-                            style: TextStyle(
-                              fontSize: 36,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Welcome to ',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
                           ),
-                          TextSpan(
-                            text: 'Bubbles!',
-                            style: TextStyle(
-                              fontSize: 36,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w500,
-                              color: const Color.fromARGB(255, 255, 0, 255),
-                            ),
+                        ),
+                        TextSpan(
+                          text: 'Bubbles!',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromARGB(255, 255, 0, 255),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Ready to pop into your first Bubble?',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w500,
-                        color: Color.fromARGB(255, 165, 165, 165),
-                      ),
+                  ),
+                  Text(
+                    'Ready to pop into your first Bubble?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 165, 165, 165),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             Positioned(
